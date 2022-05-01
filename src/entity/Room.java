f8bdec3840aa43a55eb0cb5a06daf4c8256a1c7c
Suhani172 <326346@via.dk>
2022-05-01 14:14:50 +0200

    added packages and template classes.

---
+++ src/entity/Room.java
@@ -0,0 +1,20 @@
+package entity;
+
+public class Room
+{
+  private int roomNumber;
+  private String roomType;
+  private boolean isSmokingAllowed;
+
+  public Room(int roomNumber, String roomType)
+  {
+    this.roomNumber = roomNumber;
+    this.roomType = roomType;
+    isSmokingAllowed= false;
+  }
+
+  public void setSmokingAllowed(boolean smokingAllowed)
+  {
+    isSmokingAllowed = smokingAllowed;
+  }
+}
