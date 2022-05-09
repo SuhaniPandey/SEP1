32657018edea29c26cbd7d58ffdfe8110b184f36
Devlin <325592@via.dk>
2022-05-09 14:49:33 +0200

    Added comments, not finished yet

--- src/entity/Room.java
+++ src/entity/Room.java
@@ -1,5 +1,10 @@
 package entity;
 
+/**
+ * A Class Containing Room Objects
+ * @author Devlin Onichuk
+ * @version 1.0
+ */
 public class Room
 {
   private boolean extraBed;
@@ -11,6 +16,9 @@ public class Room
 
   public Room(boolean extraBed, boolean isAvailable, String roomType)
   {
+    /**
+     * A Three Argument Constructor creating a new Room Object, Defaults extra bed to false and isAvailable to True
+     */
     this.extraBed = false;
     this.isAvailable = true;
     this.roomType = roomType;
@@ -18,41 +26,76 @@ public class Room
   }
   public Room()
   {
+    /**
+     * A no Argument Constructor, deafulting extraBed to false, isAvailable to true.
+     */
     this.extraBed = false;
     this.isAvailable = true;
     this.roomType = "";
     this.roomNumber = 0;
   }
-  public void setIsAvailable(boolean isAvailable)
+  /**
+   * Changes Room isAvailable to true/false.
+   */
+  public void setIsAvailable()
   {
     if(!(isAvailable))
       isAvailable = true;
     else
       isAvailable = false;
   }
-  public void setExtraBed(boolean extraBed)
+
+  /**
+   * Changes extraBed to true/false
+   */
+  public void setExtraBed()
   {
     if(!(extraBed))
       extraBed = true;
     else
       extraBed = false;
   }
+
+  /**
+   * checks the availability of the room
+   * @return the Availability of the room
+   */
   public boolean getIsAvailable()
   {
     return isAvailable;
   }
+
+  /**
+   *
+   * @return if the room needs an extra bed
+   */
   public boolean isExtraBed()
   {
     return extraBed;
   }
+
+  /**
+   *
+   * @return the type of room
+   */
   public String getRoomType()
   {
     return roomType;
   }
+
+  /**
+   *
+   * @return the room number
+   */
   public int getRoomNumber()
   {
     return roomNumber;
   }
+
+  /**
+   * sets the number of rooms based on the type of room
+   * @param roomType given in the constructor
+   */
   public void setNumberOfRooms(String roomType)
   {
     if(roomType.equals("Single Bedroom"))
@@ -65,24 +108,48 @@ public class Room
       numberOfRooms = 1;
     if(roomType.equals("Three Bedroom Suite"))
       numberOfRooms = 1;
-
   }
+
+  /**
+   * sets a new room number
+   * @param roomNumber sets room number by given int
+   */
   public void setRoomNumber(int roomNumber)
   {
     this.roomNumber = roomNumber;
   }
+
+  /**
+   * takes a String and sets it to the roomType
+   * @param roomType set by given String
+   */
   public void setRoomType(String roomType)
   {
     this.roomType = roomType;
   }
+
+  /**
+   * sets the price of a room by the given int
+   * @param price set by int
+   */
   public void setPrice(int price)
   {
     this.price = price;
   }
+
+  /**
+   *
+   * @return price
+   */
   public double getPrice()
   {
     return price;
   }
+
+  /**
+   * combines all the room information and creates a string of all the information in the room
+   * @return all the room information in a string
+   */
   public String toString()
   {
     return roomNumber + " Type: " + roomType + " Price: " + price + " Number of Rooms: " +
