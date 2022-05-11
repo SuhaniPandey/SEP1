968d86785233b887e54e4bfc4e544576b4bc1587
SuhaniPandey <iamsuhani5@gmail.com>
2022-05-11 11:13:07 +0200

    Added discount price,equals and to string method and added comments

--- src/entity/Booking.java
+++ src/entity/Booking.java
@@ -16,68 +16,136 @@ public class Booking
     this.departure = departure;
   }
 
-  public Booking setGuest(Guest guest)
+  /**
+   * A method to set guest
+   * @param guest takes in Guest
+   */
+
+  public void setGuest(Guest guest)
   {
     this.guest = guest;
-    return this;
   }
 
-  public Booking setRooms(Room rooms)
+  /**
+   * A method to set rooms
+   * @param rooms takes in Room
+   */
+
+  public void setRooms(Room rooms)
   {
     this.rooms = rooms;
-    return this;
   }
 
-  public Booking setArrival(Date arrival)
+  /**
+   * A method to set arrival date takes in Date
+   * @param arrival
+   */
+
+  public void setArrival(Date arrival)
   {
     this.arrival = arrival;
-    return this;
   }
 
-  public Booking setDeparture(Date departure)
+  /**
+   * A method to set departure date
+   * @param departure
+   */
+
+  public void setDeparture(Date departure)
   {
     this.departure = departure;
-    return this;
   }
 
+  /**
+   * A method to get guest
+   * @return guest
+   */
   public Guest getGuest()
   {
     return guest;
   }
+
+  /**
+   * A method to get rooms
+   * @return rooms
+   */
 
   public Room getRooms()
   {
     return rooms;
   }
 
+  /**
+   * A method to get arrival
+   * @return arrival
+   */
+
   public Date getArrival()
   {
     return arrival;
   }
+
+  /**
+   * A method to get departure
+   * @return departure
+   */
 
   public Date getDeparture()
   {
     return departure;
   }
 
+  /**
+   * A method to get final check out price in certain date interval
+   * @return price
+   */
+
   public double checkOutPrice()
   {
     int daysstayed = getArrival().dateInterval();
     double price=0;
-   if (getRooms().getPrice()== rooms.getPrice())
-   {
+    if (getRooms().getPrice()== rooms.getPrice())
+    {
       price=getRooms().getPrice()*daysstayed;
-   }
-   return price;
+    }
+    return price;
   }
-  /*
+
+  /**
+   * A method to get discount
+   * @param percent
+   * @return final price after discount
+   */
   public double discont(double percent)
   {
-    return
+    return rooms.getPrice()-(rooms.getPrice()*0.5);
   }
 
+  /**
+   *
+   * @return guest,rooms,arrival,departure
    */
 
+   public String toString()
+  {
+    return "Booking{" + "guest=" + guest + ", rooms=" + rooms + ", arrival="
+        + arrival + ", departure=" + departure + '}';
+  }
 
-
+  /**
+   * A method to check if a given object is an instance of a Booking object
+   * If obj is an instance of Booking,turn Object obj into a Booking object
+   * @param obj obj a given object
+   * @return true if the obj is an instance of Booking, else return false
+   */
+  public boolean equals(Object obj)
+  {
+    if (!(obj instanceof Booking))
+    {
+      return false;
+    }
+    Booking other=(Booking) obj;
+    return guest.equals(other.guest) && rooms.equals(other.rooms)
+        && arrival.equals(other.arrival) && departure.equals(other.departure);
+  }
 }
